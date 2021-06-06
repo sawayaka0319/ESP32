8a7448ab6b4a78a1142d1c2b1257ebe25d706f94
sawayaka0319 <sawayaka_e1@yahoo.co.jp>
2021-06-06 14:25:48 +0900

    test

---
+++ 3LED_3.py
@@ -0,0 +1,18 @@
+# 3つのLEDを点滅その３
+
+from machine import Pin
+import time
+
+pin_no = [13, 27, 33]
+led = {}
+
+led[0] = Pin(pin_no[0], Pin.OUT)
+led[1] = Pin(pin_no[1], Pin.OUT)
+led[2] = Pin(pin_no[2], Pin.OUT)
+
+while True:
+    for i in [0, 1, 2]:
+        led[i].on()
+        time.sleep(1)
+        led[i].off()
+        time.sleep(1)
